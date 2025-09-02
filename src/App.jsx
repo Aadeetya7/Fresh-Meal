c61f9d0d4240da5f45280d4f080af41044572354
Aditya Mali <aadityamali9119@gmail.com>
2025-09-02 20:30:33 +0530

    set up the project , included react icons , material UI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Container } from '@mui/material'
+
+function App() {
+
+  return (
+    <>
+      <Container>
+        This is a Container
+      </Container>
+    </>
+  )
+}
+
+export default App
